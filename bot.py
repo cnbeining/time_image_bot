4a432358c2e3cceeb2de8912c8dd52a8eccbf8dd
Beracira <g.beracira@gmail.com>
2018-10-14 21:32:09 -0400

    init commit. the bot works without timezone info. (default to EST)

---
+++ bot.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python3
+
+import datetime
+import time
+import os
+import pprint
+import telepot
+from telepot.loop import MessageLoop
+
+# print = pprint.PrettyPrinter().pprint
+
+def doit(chat_id):
+    temp = " 已經\n "
+    
+    temp += datetime.datetime.now().strftime("%H:%M")
+    
+    temp += "\n 了!!\n"
+    
+    print (temp)
+    
+    with open('temp.txt', 'w') as fout:
+        fout.write(temp)
+        
+    os.system('soffice --convert-to jpg temp.txt')
+    os.system('convert temp.jpg -crop 125x300+50+0 temp.jpg')
+    os.system('convert temp.jpg -resize 200% temp.jpg')
+    os.system('convert temp.jpg -crop 125x300+40+50 temp.jpg')
+    os.system('montage temp.jpg bot.jpg -tile 1x2 -geometry +0+0 temp.jpg')
+    os.system('montage left.jpg temp.jpg -tile 2x1 -geometry +0+0 temp.jpg')
+
+    fout = open('temp.jpg', 'rb')
+    print ('sending photo...')
+    bot.sendPhoto(chat_id, fout)
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    # print ('Content type: %s' % content_type)
+    print (content_type, chat_type, chat_id)
+
+    if chat_type == 'private': doit(chat_id)
+    elif content_type == 'text' and '@time_image_bot' in msg['text']:
+        doit(chat_id)
+    
+
+bot = telepot.Bot('561656143:AAHgRZbnnW4jDiYartSXCeYOtsC6EI9Z-ig')
+
+MessageLoop(bot, handle).run_as_thread()
+print ('I am listening ...')
+
+while 1:
+    time.sleep(10)
